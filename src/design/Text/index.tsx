c6f37c510c9d373e9e6306f7234df0ddabff14ab
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-12-27 16:09:24 +0200

    create text common component Relates #4

---
+++ src/design/Text/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { Text as RNEText } from "react-native-elements";
+
+type TextComponentProps = {
+  value: String;
+  style?: Object;
+  h1?: boolean;
+  h2?: boolean;
+  h3?: boolean;
+  h4?: boolean;
+  p?: boolean;
+};
+export const Text: FC<TextComponentProps> = ({
+  value,
+  ...rest
+}: TextComponentProps) => <RNEText {...rest}>{value}</RNEText>;
