d2be8f9a7c5e4d300f7597d62cfbe3b60f2f6d02
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-12-27 16:11:41 +0200

    create and style Button common component and rename designs folder into design Relates #4

---
+++ src/design/index.ts
@@ -0,0 +1,13 @@
+import { Text } from "./Text";
+import { Icon } from "./Icon";
+import { Button } from "./Button";
+import { Card } from "./Card";
+import { Image } from "./Image";
+
+export {
+    Text,
+    Icon,
+    Button,
+    Card,
+    Image
+}
