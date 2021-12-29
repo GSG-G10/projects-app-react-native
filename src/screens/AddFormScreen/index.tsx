a7958014fcd9c187248ea4716dcee08352bf320c
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-12-29 17:15:19 +0200

    creat the log in form

--- src/screens/AddFormScreen/index.tsx
+++ src/screens/AddFormScreen/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React , { useState } from "react";
 import { FC } from "react";
 import { View, ScrollView, StyleSheet } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,32 +1,100 @@
+import React from "react";
 import { FC } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import { Text, Icon } from "../../design";
+import { Text, Icon, Input } from "../../design";
 
 export const LogInScreen: FC = () => {
   return (
     <View style={styles.container}>
-      <Text value="Log In" h2 style={{marginBottom:30}}/>
+      <Text value="Log In" h2 style={{ marginBottom: 30 }} />
+      <Input
+            label="Name"
+            containerStyle={styles.inputContainer}
+            inputStyle={styles.input}
+            labelStyle={styles.labelStyle}
+            onChangeText={({ nativeEvent: { text } }) =>
+              null
+            }
+          />
+      <Input
+            label="Password"
+          containerStyle={{...styles.inputContainer , width:"60%",marginBottom: 30}}
+            inputStyle={styles.input}
+            labelStyle={styles.labelStyle}
+            onChangeText={({ nativeEvent: { text } }) =>
+              null
+            }
+          />
       <TouchableOpacity style={styles.btn}>
-        <Icon onPress={() => null} iconName={"google"} color={"black"} raised={false} />
-        <Text value="Google Account" p style={{fontSize:18}} />
+        <Icon
+          onPress={() => null}
+          iconName={"login"}
+          color={"black"}
+          raised={false}
+        />
+        <Text value="Log In" p style={{ fontSize: 18,marginBottom: 30 }} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}>
+        <Icon
+          onPress={() => null}
+          iconName={"google"}
+          color={"black"}
+          raised={false}
+        />
+        <Text value="Google Account" p style={{ fontSize: 18 }} />
       </TouchableOpacity>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    
   },
-  btn:{
-    width:"49%",
-  flexDirection:"row",
-  justifyContent:"space-between",
-  alignItems:"center",
-  color:"black",
-  backgroundColor:"white",
-  borderRadius:15,
-  padding:20,
+  btn: {
+    width: "49%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "black",
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 20,
+  },
+  coupleContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "1rem",
+  },
+  inputContainer: {
+    width: "60%",
+  },
+  inputContainerStyle: {
+    marginTop: "10px",
+    borderWidth: 0,
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingLeft: 10,
+    width: "100%",
+    height: 40,
+  },
+  labelStyle: {
+    marginVertical: 5,
+    fontWeight: 800,
+  },
+  btnsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  addMoreStyle: {
+    backgroundColor: "#fff",
+    border: "1px, rgba(90, 154, 230, 1), solid",
   },
 });
