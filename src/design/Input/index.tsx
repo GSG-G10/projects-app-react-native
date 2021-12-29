d4d22289acfd87f4d8777042897ffa9a799aeeff
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-12-29 14:38:18 +0200

    create input common component Relates #12

---
+++ src/design/Input/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import { View, TextInput } from "react-native";
+import { Text } from "..";
+
+type InputProps = {
+  label: string;
+  containerStyle?: object;
+  inputStyle?: object;
+  onChangeText: (args: any) => void;
+  labelStyle: Object;
+  keyboardType?: any;
+};
+export const Input: FC<InputProps> = ({
+  labelStyle,
+  label,
+  containerStyle,
+  inputStyle,
+  onChangeText,
+  keyboardType,
+}) => {
+  return (
+    <View style={containerStyle}>
+      <Text style={labelStyle} value={label} />
+      <TextInput
+        style={inputStyle}
+        onChange={onChangeText}
+        keyboardType={keyboardType}
+      />
+    </View>
+  );
+};
